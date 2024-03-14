907480c80f44a715f3acf13506633808a1f1febc
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-03-14 08:04:10 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         MP3 mp3;
-        mp3.showInfo();
+        //mp3.showInfo();
 
         Book book;
-        book.showInfo();
+     //   book.showInfo();
 
     }
 }
