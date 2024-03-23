011bb8d0d371dd4f992a1d6fbe648d5149ade552
phammquan <149579783+phammquan@users.noreply.github.com>
2024-03-23 13:43:06 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+
+
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
+        MP3 mp3 = new MP3("Hay", "Nhac Tre", "1234", 15000, 5 );
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Doraemon", "Thieu Nhi", "4321",30000,"Furiko", 200,"Giai Tri");
         book.showInfo();
-
     }
 }
